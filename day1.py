0d53a895907d0e05177733741cf891596ae0c14d
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-01-06 11:16:48 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,54 @@
+
+# print("Hello")
+
+# Ading two numbers
+
+"""
+This is my code
+and this is multilined comment
+and it is fine
+tsaguydcs
+cfnhdusbiudsj
+nhivf
+
+"""
+
+
+
+# a=7
+# b=6
+# print(a+b)
+
+# Subtarcting
+
+# print("Hello","Naman","Rohan",sep="&",end=" ")
+# print("Rahul")
+
+# print(5+7)
+# print("Hello"+" "+"Suman")
+
+# print("Hello \nhow are \nyou. \nhii i am neeraj")
+# print('Hello rahul"s how are you')
+
+# print("Hello   rahul\'s how \\'are you")
+
+# print("Hello i am \t\tgood")
+
+# a=7
+# b=8
+# print(a)
+
+# var1=89
+# var34567890=vub
+# shivam=78
+# shivamkumar=67
+# djfgjkfdgvjkd6743658734___="dvjbvfjd"
+
+# dfgkuy5t874u=
+# print1=89
+# print(print1)
+
+name="Shivam"
+NAME="Rohit"
+# name!=NAME
+print(name)
