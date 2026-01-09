e497ed8980349b029d22475a27f66ce9397d64ef
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-01-09 12:00:47 +0530

    Functions and lists

---
+++ day4.py
@@ -0,0 +1,80 @@
+
+# inp1=6
+# inp2=5
+# print(inp1+inp2)
+
+# inp3=9
+# inp4=7
+# print(inp3+inp4)
+
+# def addTwoNum(num1,num2): # formal parameters
+#     print(num1+num2)
+
+
+# addTwoNum(4,6) # actual parameters
+# addTwoNum(7,5)
+
+# def greet(name="Guest"):
+#     print("Hello Good Morning",name)
+
+# greet()
+# greet()
+# greet()
+# greet()
+
+# greet("Jazaul")
+
+
+
+# def fact(num1):
+#     factVal=1
+#     for i in range(1,num1+1):
+#         factVal=factVal*i
+#     print("Factorial val is: ",factVal)
+#     return factVal
+
+# fact(5)
+# print(fact(4))
+
+# list1=[4,7,3,8,3,"Sohan",True]
+
+# list1[3]="Rohan"
+# print(list1[5])
+# print(list1[0])
+# print(list1)
+# print(list1[1:5])
+# print(list1[-3:-1])
+# print(list1[67])
+# for item in list1:
+#     print(item)
+
+# colors = [5,3,8,2,3]
+# colors.sort()
+# colors.sort(reverse=True)
+# colors.reverse()
+# print(colors)
+# print(colors.index(3))
+# print(colors.count(3))
+
+# x=[3,6,8]
+
+# y=x.copy()
+
+# x[1]=78
+
+# print(x)
+# print(y)
+
+# app1=[5,7,3]
+# app1.append(6)
+# app1.append(56)
+
+# app1.insert(2,67)
+
+# print(app1)
+
+# app1=[5,7,3]
+# app2=[15,17,13]
+
+# app1.extend(app2)
+# print(app1+app2)
