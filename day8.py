e5b51247ed614a4f620ed61b02f5fafc5036b5f9
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-01-15 11:51:02 +0530

    Opps concepts

---
+++ day8.py
@@ -0,0 +1,94 @@
+
+# # Employee 1
+
+# empId1=101
+# empName1="Gaurav"
+# empSalary1=80000
+
+# # Employee 2
+
+# empId2=101
+# empName2="Mohan"
+# empSalary2=45000
+
+# # Employee 3
+
+# empId3=101
+# empName3="Raghav"
+# empSalary3=90000
+
+# class Employee:
+#     empId=0
+#     name="Guest"
+#     empSalary=0
+
+# Rohan=Employee()
+# Mohan=Employee()
+
+# Rohan.empId=101
+# Rohan.name="Rohan"
+# Rohan.empSalary=25000
+
+# Mohan.empId=102
+# Mohan.name="Mohan"
+# Mohan.empSalary=35000
+
+# print(Rohan.empId)
+# print(Mohan.empId)
+
+# class Employee:
+#     def __init__(self,id,name,salary):
+#         # print("Hello Constructor")
+#         self.id=id
+#         self.name=name
+#         self.salary=salary
+
+
+# rohan=Employee(101,"Rohan",25000)
+# john=Employee(102,"John",35000)
+# peter=Employee(103,"Peter",24000)
+
+
+# print(rohan.id)
+# print(rohan.name)
+# print(rohan.salary)
+
+# print(john.id)
+# print(john.name)
+# print(john.salary)
+
+
+
+# class Employee:
+#     def __init__(self):
+#         print("Hello Constructor")
+
+# var1=Employee()
+
+# class Animal:
+#     def eat(self):
+#         print("Animal is Eating")
+
+#     def walk(self):
+#         print("Animal is walking")
+
+
+
+# class Horse(Animal):
+#     def run(self):
+#         print("Horse is running")
+#     def drink(self):
+#         print("Horse is drinking")
+
+
+# obj1=Animal()
+
+# obj1.eat()
+# obj1.run()
+
+# obj2=Horse()
+
+# obj2.run()
+# obj2.eat()
+# obj2.walk()
+# obj2.drink()
